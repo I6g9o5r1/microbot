46752c6841665b98911c45560cc6e902df71e23a
g2334i15 <101813482+I6g9o5r1@users.noreply.github.com>
2025-06-03 20:08:25 +0700

    Update code.py

--- code.py
+++ code.py
@@ -14,6 +14,12 @@ def on_uart_data_received():
         b()
     elif data == "AC-1":
         sound()
+    elif data == "AC1":
+        strip.show_color(neopixel.colors(NeoPixelColors.RED))
+        strip.show()
+    elif data == "AU-1":
+        strip.clear()
+        strip.show()
 bluetooth.on_uart_data_received(serial.delimiters(Delimiters.HASH), on_uart_data_received)
 #функции робота
 def f():
@@ -21,17 +27,21 @@ def f():
     robotbit.motor_run(robotbit.Motors.M2A, 200)
     robotbit.motor_run(robotbit.Motors.M1B, 200)
     robotbit.motor_run(robotbit.Motors.M2B, 200)
+    s()
 def r():
     robotbit.motor_run(robotbit.Motors.M1A, -200)
     robotbit.motor_run(robotbit.Motors.M2A, 200)
     robotbit.motor_run(robotbit.Motors.M1B, -200)
     robotbit.motor_run(robotbit.Motors.M2B, 200)
+    s()
 def l():
     robotbit.motor_run(robotbit.Motors.M1A, 200)
     robotbit.motor_run(robotbit.Motors.M2A, -200)
     robotbit.motor_run(robotbit.Motors.M1B, 200)
     robotbit.motor_run(robotbit.Motors.M2B, -200)
+    s()
 def s():
+    basic.pause(100)
     robotbit.motor_run(robotbit.Motors.M1A, 0)
     robotbit.motor_run(robotbit.Motors.M2A, 0)
     robotbit.motor_run(robotbit.Motors.M1B, 0)
@@ -41,6 +51,7 @@ def b():
     robotbit.motor_run(robotbit.Motors.M2A, -200)
     robotbit.motor_run(robotbit.Motors.M1B, -200)
     robotbit.motor_run(robotbit.Motors.M2B, -200)
+    s()
 def sound():
     music.play(music.create_sound_expression(WaveShape.NOISE,
                     4980,
@@ -53,3 +64,6 @@ def sound():
                 music.PlaybackMode.UNTIL_DONE)
 data = ""
 bluetooth.start_uart_service()
+basic.show_icon(IconNames.HAPPY)
+strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+range2 = strip.range(0, 4)
